0d8ae7ad5f45bc09dce6ae1d9c7d8bf3f04ab507
Robin Brumbter <brumbter1u@etu.univ-lorraine.fr>
2023-01-27 17:11:01 +0100

    Creating work file

---
+++ src/profil/work.js
@@ -0,0 +1,108 @@
+// List of link types
+const BA = "Bande-annonce";
+const RE = "Résumé";
+
+// Work list
+module.exports = {
+
+    /***************************
+     * title: 
+     *      Name of the content
+     * description: 
+     *      A short description of the work
+     * date: 
+     *      When the work was finished - displayed as written
+     * link: 
+     *      A link for the user to see more details (youtube, wikipedia...)
+     * type_link: 
+     *      What will be written on the button for the link
+     *      -> Variables to use are on the top of the document
+     * affiche: 
+     *      The name of the picture file (without the extension)
+     ***************************/
+
+    doublages: [{
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },
+    {
+        title: "Oeuvre",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },],
+
+    sous_titrages: [{
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },
+    {
+        title: "Oeuvre",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },],
+
+    voice_over: [{
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },
+    {
+        title: "Oeuvre",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },],
+
+    jeux_video: [{
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },
+    {
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },],
+
+    sme: [{
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },
+    {
+        title: "Lookism",
+        description: "Script FR épisodes 1 et 2 dqzhdnbyhzqdh zuyqhd uzqhd uzhu dhzu dzud z huz hu zhuzh uefe",
+        date: "12/2022",
+        link: "https://www.google.com/",
+        type_link: BA,
+        affiche: "imgDoublage1",
+    },],
+}
